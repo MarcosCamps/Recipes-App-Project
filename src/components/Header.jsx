461cbff1421ebdac598075435cdd93de59df1386
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-04-26 19:37:08 -0300

    Adiciona o input e os radio buttons e fetchs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchSearchByIngredient,
+  fetchSearchByName, fetchByFirstLetter } from '../services/apiRequests';
 
-function Header() {
+function Header({ Title }) {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchType, setSearchType] = useState('');
+
+  function onSearchClick() {
+    switch (searchType) {
+    case 'ingredient':
+      return fetchSearchByIngredient(searchInput, Title);
+    case 'name':
+      return fetchSearchByName(searchInput, Title);
+    case 'first-letter':
+      return fetchByFirstLetter(searchInput, Title);
+    default:
+      break;
+    }
+  }
+
   return (
-    <h1>Testando a pasta do Header</h1>
+    <section className="search-container">
+      <input
+        type="text"
+        data-testid="search-input"
+        value={ searchInput }
+        onChange={ ({ target: { value } }) => setSearchInput(value) }
+      />
+      <div
+        name="search-type"
+        value={ searchType }
+        onChange={ ({ target: { value } }) => setSearchType(value) }
+      >
+        <label htmlFor="ingredient-search">
+          Ingrediente
+          <input
+            type="radio"
+            name="search-type"
+            id="ingredient-search"
+            data-testid="ingredient-search-radio"
+            value="ingredient"
+          />
+        </label>
+        <label htmlFor="name-search">
+          Nome
+          <input
+            type="radio"
+            name="search-type"
+            id="name-search"
+            data-testid="name-search-radio"
+            value="name"
+          />
+        </label>
+        <label htmlFor="first-letter-search">
+          Primeira Letra
+          <input
+            type="radio"
+            name="search-type"
+            id="first-letter-search"
+            data-testid="first-letter-search-radio"
+            value="first-letter"
+          />
+        </label>
+      </div>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ onSearchClick }
+      >
+        Search
+      </button>
+    </section>
   );
 }
 
---
+++ src/services/apiRequests.js
@@ -0,0 +1,27 @@
+async function handleFetch(endpoint) {
+  const fetchAPi = await fetch(endpoint);
+  const response = await fetchAPi.json();
+  console.log(response);
+  return response;
+}
+
+export async function fetchSearchByIngredient(ingredient, type) {
+  const drinkEndpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient};`;
+  const foodEndpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
+  if (type === 'Foods') return handleFetch(foodEndpoint);
+  if (type === 'Drinks') return handleFetch(drinkEndpoint);
+}
+
+export async function fetchSearchByName(name, type) {
+  const foodEndpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+  const drinkEndpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+  if (type === 'Foods') return handleFetch(foodEndpoint);
+  if (type === 'Drinks') return handleFetch(drinkEndpoint);
+}
+
+export async function fetchByFirstLetter(firstLetter, type) {
+  const foodEndpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`;
+  const drinkEndpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}`;
+  if (type === 'Foods') return handleFetch(foodEndpoint);
+  if (type === 'Drinks') return handleFetch(drinkEndpoint);
+}
