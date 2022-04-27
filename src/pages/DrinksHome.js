f504825298ed484b59f4913900d58d01208c032e
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-04-27 19:19:09 -0300

    Adiciona os requisitos 25 e 26

--- src/pages/DrinksHome.js
+++ src/pages/DrinksHome.js
@@ -1,20 +1,34 @@
-import React, { useContext } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import recipesContext from '../context/RecipesContext';
+import { fetchSearchByName } from '../services/apiRequests';
+import '../Styles/home.css';
 
 const limit = 12;
 
 function DrinksHome() {
   const {
     recipes,
+    setRecipes,
     isSearching,
+    setIsSearching,
   } = useContext(recipesContext);
+
+  useEffect(() => {
+    const recipesFetch = async () => {
+      const response = await fetchSearchByName('', 'Drinks');
+      setRecipes(response);
+      setIsSearching(true);
+    };
+    recipesFetch();
+  }, []);
 
   return (
     <>
       <Header Title="Drinks" />
-      <section>
+      <section className="recipes-container">
         { isSearching && recipes.drinks.slice(0, limit).map((recipe, index) => (
           <div data-testid={ `${index}-recipe-card` } key={ index }>
             <img
--- src/pages/FoodHome.js
+++ src/pages/FoodHome.js
@@ -1,20 +1,34 @@
-import React, { useContext } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import recipesContext from '../context/RecipesContext';
+import { fetchSearchByName } from '../services/apiRequests';
+import '../Styles/home.css';
 
 const limit = 12;
 
 function FoodHome() {
   const {
     recipes,
+    setRecipes,
     isSearching,
+    setIsSearching,
   } = useContext(recipesContext);
+
+  useEffect(() => {
+    const recipesFetch = async () => {
+      const response = await fetchSearchByName('', 'Foods');
+      setRecipes(response);
+      setIsSearching(true);
+    };
+    recipesFetch();
+  }, []);
 
   return (
     <>
       <Header Title="Foods" />
-      <section>
+      <section className="recipes-container">
         { isSearching && recipes.meals.slice(0, limit).map((recipe, index) => (
           <div data-testid={ `${index}-recipe-card` } key={ index }>
             <img
