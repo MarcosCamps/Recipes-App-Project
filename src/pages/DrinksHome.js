cce9e7a2da3d5c2cd3191b1972ef215e33fe819f
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-04-27 18:12:20 -0300

    Tornando o resultado global.

--- src/pages/DrinksHome.js
+++ src/pages/DrinksHome.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import recipesContext from '../context/RecipesContext';
+
+const limit = 12;
 
 function DrinksHome() {
+  const {
+    recipes,
+    isSearching,
+  } = useContext(recipesContext);
+
   return (
     <>
       <Header Title="Drinks" />
-      <h1>Tela de DrinksHome</h1>
+      <section>
+        { isSearching && recipes.drinks.slice(0, limit).map((recipe, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ index }>
+            <img
+              src={ recipe.strDrinkThumb }
+              alt={ recipe.strDrinkThumb }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{recipe.strDrink}</p>
+          </div>
+        ))}
+      </section>
       <Footer />
     </>
   );
--- src/pages/FoodHome.js
+++ src/pages/FoodHome.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import recipesContext from '../context/RecipesContext';
+
+const limit = 12;
 
 function FoodHome() {
+  const {
+    recipes,
+    isSearching,
+  } = useContext(recipesContext);
+
   return (
     <>
       <Header Title="Foods" />
+      <section>
+        { isSearching && recipes.meals.slice(0, limit).map((recipe, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ index }>
+            <img
+              src={ recipe.strMealThumb }
+              alt={ recipe.strMealThumb }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{recipe.strMeal}</p>
+          </div>
+        ))}
+      </section>
       <Footer />
     </>
   );
