aba22a95b1a6afa99554ab10c8692dac9715e6f3
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-04-28 13:55:23 -0300

    Fix: Resolve o bug do alert

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
@@ -10,59 +10,56 @@ import recipesContext from '../context/RecipesContext';
 
 function SearchBar({ Title }) {
   const {
-    recipes,
     setRecipes,
     setIsSearching,
   } = useContext(recipesContext);
   const [searchInput, setSearchInput] = useState('');
   const [searchType, setSearchType] = useState('');
-  /* const [isSearchingDrink, setIsSearchingDrink] = useState(false);
-  const [isSearchingFood, setIsSearchingFood] = useState(false); */
   const history = useHistory();
 
-  function foodDetailsPush() {
-    const { meals } = recipes;
-    console.log(recipes);
-    console.log(meals);
+  function foodDetailsPush(response) {
+    const { meals } = response;
     if (meals.length === 1) {
       history.push(`/foods/${meals[0].idMeal}`);
     }
   }
-  function drinkDetailsPush() {
-    const { drinks } = recipes;
-    console.log(recipes);
-    console.log(drinks);
+  function drinkDetailsPush(response) {
+    const { drinks } = response;
     if (drinks.length === 1) {
       history.push(`/drinks/${drinks[0].idDrink}`);
     }
   }
 
-  function showRecipesList() {
-    const type = Object.keys(recipes)[0];
-    if (recipes[type]) {
-      if (Object.keys(recipes)[0] === 'meals') {
-        foodDetailsPush();
+  function oneMealOnly(response) {
+    const type = Object.keys(response)[0];
+    if (response[type]) {
+      if (Object.keys(response)[0] === 'meals') {
+        foodDetailsPush(response);
       }
-      if (Object.keys(recipes)[0] === 'drinks') {
-        drinkDetailsPush();
+      if (Object.keys(response)[0] === 'drinks') {
+        drinkDetailsPush(response);
       }
     }
-    if (!recipes[type]) {
+    if (response[type].length === 0) {
       global.alert('Sorry, we haven\'t found any recipes for these filters.');
-      setRecipes({ [type]: [] });
     }
   }
 
   async function onSearchClick() {
+    const mealType = Title === 'Foods' ? 'meals' : 'drinks';
     switch (searchType) {
     case 'ingredient': {
       const response = await fetchSearchByIngredient(searchInput, Title);
+      response[mealType] ??= [];
+      oneMealOnly(response);
       setRecipes(response);
       setIsSearching(true);
       break;
     }
     case 'name': {
       const response = await fetchSearchByName(searchInput, Title);
+      response[mealType] ??= [];
+      oneMealOnly(response);
       setRecipes(response);
       setIsSearching(true);
       break;
@@ -72,6 +69,9 @@ function SearchBar({ Title }) {
         global.alert('Your search must have only 1 (one) character');
       }
       const response = await fetchByFirstLetter(searchInput, Title);
+      response[mealType] ??= [];
+      console.log(response);
+      oneMealOnly(response);
       setRecipes(response);
       setIsSearching(true);
       break;
@@ -80,10 +80,6 @@ function SearchBar({ Title }) {
       break;
     }
   }
-
-  useEffect(() => {
-    showRecipesList();
-  }, [recipes]);
 
   return (
     <div>
@@ -138,28 +134,6 @@ function SearchBar({ Title }) {
           Search
         </button>
       </section>
-      {/* <section>
-        { isSearchingDrink && recipes.drinks.slice(0, limit).map((recipe, index) => (
-          <div data-testid={ `${index}-recipe-card` } key={ index }>
-            <img
-              src={ recipe.strDrinkThumb }
-              alt={ recipe.strDrinkThumb }
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{recipe.strDrink}</p>
-          </div>
-        ))}
-        { isSearchingFood && recipes.meals.slice(0, limit).map((recipe, index) => (
-          <div data-testid={ `${index}-recipe-card` } key={ index }>
-            <img
-              src={ recipe.strMealThumb }
-              alt={ recipe.strMealThumb }
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{recipe.strMeal}</p>
-          </div>
-        ))}
-      </section> */}
     </div>
   );
 }
