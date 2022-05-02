073ffd533507e3e53b63257dbcf9ded215c5c163
Leonardo Silva <leonardorc.geo@gmail.com>
2022-05-02 15:00:26 -0300

    Requisitos 70-74

--- src/pages/FoodExplore.js
+++ src/pages/FoodExplore.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { randomAPIMeal } from '../services/apiRequests';
 
-function FoodExplore() {
+function FoodExplore({ history }) {
+  const handleRandomMeal = async () => {
+    const randomMeal = await randomAPIMeal();
+    console.log(randomMeal.meals[0].idMeal);
+    history.push(`/foods/${randomMeal.meals[0].idMeal}`);
+  };
   return (
-    <>
+    <div>
       <Header Title="Explore Foods" />
-      <h1>Tela de FoodExplore</h1>
+      <section>
+        <Link to="/explore/foods/ingredients">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            By Ingredient
+          </button>
+        </Link>
+        <Link to="/explore/foods/nationalities">
+          <button
+            type="button"
+            data-testid="explore-by-nationality"
+          >
+            By Nationality
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => handleRandomMeal() }
+        >
+          Surprise me!
+        </button>
+      </section>
       <Footer />
-    </>
+    </div>
   );
 }
+
+FoodExplore.propTypes = {
+  history: PropTypes.objectOf(Object).isRequired,
+};
 
 export default FoodExplore;
