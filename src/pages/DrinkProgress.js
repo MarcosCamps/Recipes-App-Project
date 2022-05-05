51f04fbbfec826d694d57ea5d27d399cb38bba42
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-05-05 17:23:16 -0300

    requisito 47, 48, 49 ok

--- src/pages/DrinkProgress.js
+++ src/pages/DrinkProgress.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { fetchForID } from '../services/apiRequests';
+import CheckboxProgress from '../components/FoodProgress/checkbox';
 
-function DrinkProgress() {
+function DrinkProgress(props) {
+  const { match: { params: { id } } } = props;
+  const [informations, setInformations] = useState('');
+
+  useEffect(() => {
+    const informationFetch = async () => {
+      const response = await fetchForID('drinks', id);
+      setInformations(response.drinks[0]);
+    };
+    informationFetch();
+  }, [id]);
+
+  const Ingredients = () => {
+    const maxIngredients = 20;
+    const ingredients = [];
+    for (let index = 1; index <= maxIngredients; index += 1) {
+      ingredients.push(informations[`strIngredient${index}`]);
+    }
+    return ingredients;
+  };
+
   return (
-    <h1>Tela de DrinkProgress</h1>
+    informations === '' ? <p>Carregando</p> : (
+      <div>
+        <img
+          src={ informations.strDrinkThumb }
+          alt="photoMeal"
+          data-testid="recipe-photo"
+          width="300px"
+        />
+        <h2 data-testid="recipe-title">{ informations.strDrink }</h2>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="favorite-btn">Favorite</button>
+        <h3 data-testid="recipe-category">{ informations.strCategory }</h3>
+        <CheckboxProgress
+          Ingredients={ Ingredients }
+          id={ id }
+          type="cocktails"
+          ofType="meals"
+        />
+        <h4 data-testid="instructions">{ informations.strInstructions }</h4>
+        <button data-testid="finish-recipe-btn" type="button">Finalizar receita</button>
+      </div>
+    )
   );
 }
+
+DrinkProgress.propTypes = {
+  match: PropTypes.string.isRequired,
+};
 
 export default DrinkProgress;
--- src/pages/FoodProgress.js
+++ src/pages/FoodProgress.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { fetchForID } from '../services/apiRequests';
+import CheckboxProgress from '../components/FoodProgress/checkbox';
 
-function FoodProgress() {
+function FoodProgress(props) {
+  const { match: { params: { id } } } = props;
+  const [informations, setInformations] = useState('');
+
+  useEffect(() => {
+    const informationFetch = async () => {
+      const response = await fetchForID('meals', id);
+      setInformations(response.meals[0]);
+    };
+    informationFetch();
+  }, [id]);
+
+  const Ingredients = () => {
+    const maxIngredients = 20;
+    const ingredients = [];
+    for (let index = 1; index <= maxIngredients; index += 1) {
+      ingredients.push(informations[`strIngredient${index}`]);
+    }
+    return ingredients;
+  };
+
   return (
-    <h1>Tela de FoodProgress</h1>
+    informations === '' ? <p>Carregando</p> : (
+      <div>
+        <img
+          src={ informations.strMealThumb }
+          id="photo-progress"
+          width="300px"
+          alt="photoMeal"
+          data-testid="recipe-photo"
+        />
+        <h2 data-testid="recipe-title">{ informations.strMeal }</h2>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="favorite-btn">Favorite</button>
+        <h3 data-testid="recipe-category">{ informations.strCategory }</h3>
+        <CheckboxProgress
+          Ingredients={ Ingredients }
+          id={ id }
+          type="meals"
+          ofType="cocktails"
+        />
+        <h4 data-testid="instructions">{ informations.strInstructions }</h4>
+        <button data-testid="finish-recipe-btn" type="button">Finalizar receita</button>
+      </div>
+    )
   );
 }
+
+FoodProgress.propTypes = {
+  match: PropTypes.objectOf(PropTypes.object).isRequired,
+};
 
 export default FoodProgress;
